25334de1e8c8c131325673d8086b47a10cbeaab2
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 16:32:17 +0000

    Automatically backed up by Learn

--- testing.rb
+++ testing.rb
@@ -16,20 +16,4 @@ pigeon_data = {
     "City Hall" => ["Andrew"]
   }
 }
-pigeon_names = []
-attributes = {}
-pigeon_list = {}
 
-# get names
-pigeon_data.each do |(key, value)|
-  attributes << key
-  value.each do |k, v|
-  pigeon_names.concat(v)
-  end
-end
-pigeon_names = pigeon_names.uniq
-
-# add names as keys to the new hash
-pigeon_list = Hash[pigeon_names.collect {|element| [element, {}]}]
-pigeon_list.each {|(key, value)| key[value]}
-p pigeon_list
