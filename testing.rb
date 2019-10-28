636f8621fb0d42308434bf0c51b585fb24037b78
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 16:32:59 +0000

    Automatically backed up by Learn

--- testing.rb
+++ testing.rb
@@ -17,3 +17,15 @@ pigeon_data = {
   }
 }
 
+def grab_pigeon_names(pigeon_hash)
+  pigeon_names = []
+  pigeon_hash.each do |key, value|
+    key.each do |key, value|
+      value.each {|element| pigeon_names << element}
+    end
+  end
+  pigeon_names
+end
+
+names = grab_pigeon_names(pigeon_data)
+pp names
