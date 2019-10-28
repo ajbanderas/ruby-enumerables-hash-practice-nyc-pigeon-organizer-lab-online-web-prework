f7b7f05fa89eb041aa109e07298ce98aad9a69f8
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 17:08:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,25 +16,25 @@ def create_new_hash(key_array)
   new_hash
 end
 
-def set_new_values(source, target)
-  target.each do |key, value|
-    name = key
-    source.each do |key, value|
-      attribute = key
-      value.each do |key, value|
-        specific_attribute = key
-        if value.include?(name)
-          name[attribute] << specific_attribute
-        end
-      end
-    end  
-  end
-end
+#def set_new_values(source, target)
+ # target.each do |key, value|
+  #  name = key
+   # source.each do |key, value|
+    #  attribute = key
+     # value.each do |key, value|
+      #  specific_attribute = key
+       # if value.include?(name)
+        #  name[attribute] << specific_attribute
+        #end
+      #end
+    #end  
+  #end
+#end
 
 def nyc_pigeon_organizer(data)
   organized_hash = {}
   names = grab_pigeon_names(data)
   organized_hash = create_new_hash(names)
-  set_new_values(data, organized_hash)
+  pp organized_hash
 end
 
