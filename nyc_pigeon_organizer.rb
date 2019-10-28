46fa376926ecb7585a59f4784320f9ed4e8a006b
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 16:29:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,10 @@ def grab_pigeon_names(pigeon_hash)
   pigeon_names = []
   pigeon_hash.each do |key, value|
     key.each do |key, value|
-      
+      value.each {|element| pigeon_names << element}
     end
   end
+  pp pigeon_names
 end
 
 def nyc_pigeon_organizer(data)
